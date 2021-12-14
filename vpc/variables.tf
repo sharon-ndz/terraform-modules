78e6694b7e3000a3d02359884022a2391381b6db
halfadywageeh <fady.wageeh@halliburton.com>
2021-12-14 16:23:44 +0200

    organize &7 add missing variables

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,81 +1,73 @@
-variable "vpc_cidr" {
-  type = string
+ ###General###
+variable "environment" {}
+variable "region" {
+  default = ""
 }
-
-variable "instance_tenancy" {
-  type = string
-  default = "default"
-}
-
-variable "enable_dns_support" {
-  type = bool
-}
-
-variable "enable_dns_hostnames" {
-  type = bool
-}
-
-variable "vpc_name" {
-  type = string
-}
-
 variable "common_tags" {
   type = map
 }
 
+###VPC###
+variable "instance_tenancy" {
+  type = string
+  default = "default"
+}
+variable "enable_dns_support" {
+  type = bool
+}
+variable "enable_dns_hostnames" {
+  type = bool
+}
+variable "vpc_name" {
+  type = string
+}
+variable "vpc_cidr" {
+  type = string
+}
+
+###IGW###
 variable "internet_gateway_name" {
   type = string
 }
 
-variable "vpc_public_subnet_cidr" {
-  type = list
-}
-
-variable "map_public_ip_on_launch" {
-  type = bool
-}
-
-
-variable "public_subnets_name" {
-  type = string
-}
-
-variable "extra_public_routes" {
-  type = list
-}
-
-variable "public_table_name" {
-  type = string
-}
-
-variable "vpc_public_subnet_cidr" {
-  type = list
-}
-
-variable "vpc_private_subnets" {
-  type = list
-}
-
+###NGW###
 variable "total_nat_gateway_required" {
   type = number
 }
-
 variable "eip_for_nat_gateway_name" {
   type = string
 }
-
 variable "nat_gateway_name" {
   type = string
 }
 
+###Private Subnets###
+variable "private_subnet_name" {
+  type = string
+}
+variable "vpc_private_subnets" {
+  type = list
+}
 variable "private_route_cidr" {
   type = string
 }
-
 variable "private_route_table_name" {
   default = ""
 }
 
-variable "region" {
-  default = ""
+###Public Subnets###
+variable "map_public_ip_on_launch" {
+  type = bool
+}
+variable "public_subnets_name" {
+  type = string
+}
+variable "extra_public_routes" {
+  type = list
+}
+variable "public_route_table_name" {
+  type = string
+}
+variable "vpc_public_subnet_cidr" {
+  type = list
 }
