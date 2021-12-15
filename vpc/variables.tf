675d02e5a3c8d07ae07da7ab7651f3c609db659f
halfadywageeh <fady.wageeh@halliburton.com>
2021-12-15 11:16:46 +0200

    remove unused variable

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -42,32 +42,25 @@ variable "nat_gateway_name" {
 }
 
 ###Private Subnets###
-variable "private_subnet_name" {
-  type = string
-}
-variable "vpc_private_subnets" {
-  type = list
-}
-variable "private_route_cidr" {
-  type = string
-}
-variable "private_route_table_name" {
-  default = ""
+variable "private_subnets" {
+  type = object({
+    routes                   = list
+    cidrs_blocks             = list(string)
+    subnets_name_prefix      = string
+    route_table_name         = string
+  })
 }
 
 ###Public Subnets###
 variable "map_public_ip_on_launch" {
   type = bool
 }
-variable "public_subnets_name" {
-  type = string
-}
-variable "extra_public_routes" {
-  type = list
-}
-variable "public_route_table_name" {
-  type = string
-}
-variable "vpc_public_subnet_cidr" {
-  type = list
+variable "public_subnets" {
+  type = object({
+    routes                   = list
+    cidrs_blocks             = list(string)
+    subnets_name_prefix      = string
+    map_public_ip_on_launch  = bool
+    route_table_name         = string
+  })
 }
