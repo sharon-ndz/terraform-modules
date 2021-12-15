b060aa2325fe209388073e6bdff6c2f9dc0d4ba2
halfadywageeh <fady.wageeh@halliburton.com>
2021-12-15 21:00:55 +0200

    update variables

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -14,39 +14,31 @@ variable "instance_tenancy" {
 }
 variable "enable_dns_support" {
   type = bool
-  default = false
 }
 variable "enable_dns_hostnames" {
   type = bool
-  default = false
 }
 variable "vpc_name" {
   type = string
-  default = ""
 }
 variable "vpc_cidr" {
   type = string
-  default = ""
 }
 
 ###IGW###
 variable "internet_gateway_name" {
   type = string
-  default = ""
 }
 
 ###NGW###
 variable "total_nat_gateway_required" {
   type = number
-  default = 0
 }
 variable "eip_for_nat_gateway_name" {
   type = string
-  default = ""
 }
 variable "nat_gateway_name" {
   type = string
-  default = ""
 }
 
 ###Private Subnets###
@@ -57,7 +49,6 @@ variable "private_app_subnets" {
     subnets_name_prefix      = string
     route_table_name         = string
   })
-  default = {}
 }
 
 ##Private Data Subnets
@@ -68,7 +59,6 @@ variable "private_data_subnets" {
     subnets_name_prefix      = string
     route_table_name         = string
   })
-  default = {}
 }
 
 ##Private Services Subnets
@@ -79,7 +69,6 @@ variable "private_services_subnets" {
     subnets_name_prefix      = string
     route_table_name         = string
   })
-  default = {}
 }
 
 ###Public Subnets###
@@ -91,5 +80,4 @@ variable "public_subnets" {
     map_public_ip_on_launch  = bool
     route_table_name         = string
   })
-  default = {}
 }
