3f03c2d0cde5abaf3a8527c85e21a83662cccbc2
halfadywageeh <fady.wageeh@halliburton.com>
2021-12-20 17:21:15 +0200

    add ec2 generic module initial code

---
+++ ec2/main.tf
@@ -0,0 +1,180 @@
+locals {
+  volumesLen = length(var.volumes)
+}
+
+data "aws_iam_policy_document" "instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ec2_iam_role" {
+  name               = var.instance_profile_name
+  assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
+
+  dynamic "inline_policy" {
+    for_each = var.machine_iam_policies
+
+    content {
+        name = lookup(inline_policy.value, "name", null)
+
+        policy = jsonencode({
+          Version = "2012-10-17"
+          Statement = [
+            {
+              Action   = lookup(inline_policy.value, "action", null)
+              Effect   = lookup(inline_policy.value, "effect", null)
+              Resource = lookup(inline_policy.value, "resource", null)
+            },
+          ]
+        })
+    }
+  }
+}
+
+resource "aws_security_group" "ec2_security_group" {
+  count       = var.create_sg ? 1 : 0
+  name        = "ec"
+  description = "EC2 SG"
+  vpc_id      = var.vpc_id
+
+  dynamic "ingress" {
+    for_each = var.ingress_roles
+    content {
+        description         = lookup(ingress.value, "description", null)
+        from_port           = lookup(ingress.value, "from_port", null)
+        to_port             = lookup(ingress.value, "to_port", null)
+        protocol            = lookup(ingress.value, "protocol", null)
+        cidr_blocks         = lookup(ingress.value, "cidr_blocks", null)
+        ipv6_cidr_blocks    = lookup(ingress.value, "ipv6_cidr_blocks", null)
+    }
+  }
+
+  dynamic "egress" {
+    for_each = var.egress_roles
+    content {
+        description         = lookup(egress.value, "description", null)
+        from_port           = lookup(egress.value, "from_port", null)
+        to_port             = lookup(egress.value, "to_port", null)
+        protocol            = lookup(egress.value, "protocol", null)
+        cidr_blocks         = lookup(egress.value, "cidr_blocks", null)
+        ipv6_cidr_blocks    = lookup(egress.value, "ipv6_cidr_blocks", null)
+    }
+  }
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = var.instance_profile_name
+  role = aws_iam_role.ec2_iam_role.name
+}
+
+resource "aws_instance" "ec2_instance" {
+
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  private_ip                  = var.private_ip
+  secondary_private_ips       = var.secondary_private_ips
+  vpc_security_group_ids      = concat([aws_security_group.ec2_security_group.id], var.secuity_groups_ids)
+  key_name                    = var.key_pair
+  associate_public_ip_address = var.associate_public_ip_address
+  disable_api_termination     = var.disable_api_termination
+
+  root_block_device {
+    volume_size           = var.root_volume_size
+    volume_type           = var.root_volume_type
+    delete_on_termination = var.root_volume_delete_on_termination
+  }
+  
+  tags = merge(
+    {
+      "Name" = var.instance_name
+    },
+    var.common_tags,
+    var.extra_tags,
+  )
+
+
+  user_data = var.is_linux ? var.script : <<EOF
+    <powershell>
+    $len=${local.volumesLen}
+    $csvValue="${var.disks_list}"
+    if ($csvValue)
+    {
+      if( $csvValue.Contains("|") ) {
+        $filename = "$env:TEMP\input.csv"
+        $result= $csvValue.split('|')  
+        $result | out-file $filename 
+        $disks =  Import-Csv $filename -Header "Disk", "Letter", "Label"
+      
+        for ($num = 0 ; $num -lt $len; $num++) {  
+          $letter =  $disks.letter.get($num)
+          $diskNum =  $disks.disk.get($num)
+          $diskLabel = $disks.label.get($num)
+          $text="select disk $diskNum `n attributes disk clear readonly `n convert mbr `n create partition primary `n format quick fs=ntfs label=$diskLabel `n assign letter=$letter"
+          $text  |ForEach-Object {$_ -Replace 'Warning', 'Caution'} |
+              Set-Content -Path $env:TEMP\disk$diskNum.txt
+        diskpart /s  $env:TEMP\disk$diskNum.txt
+          }
+
+      }else {
+        $filename = "$env:TEMP\input.csv"
+        $csvValue | out-file $filename  
+        $disks =  Import-Csv $filename -Header "Disk", "Letter", "Label" 
+        
+        for ($num = 0 ; $num -lt $len; $num++) {  
+          $letter =  $disks.letter
+          $diskNum =  $disks.disk
+          $diskLabel = $disks.label
+          $text="select disk $diskNum `n attributes disk clear readonly `n convert mbr `n create partition primary `n format quick fs=ntfs label=$diskLabel `n assign letter=$letter"
+          $text  |ForEach-Object {$_ -Replace 'Warning', 'Caution'} |
+              Set-Content -Path $env:TEMP\disk$diskNum.txt
+        diskpart /s  $env:TEMP\disk$diskNum.txt
+          } 
+      }
+    }
+    ${var.script}
+    
+    </powershell>
+  EOF 
+  lifecycle {
+    ignore_changes = [ami, tags, user_data, root_block_device, iam_instance_profile]
+
+  }
+}
+
+resource "aws_ebs_volume" "volume" {
+  count             = length(var.volumes)
+  availability_zone = aws_instance.ec2_instance.availability_zone
+  size              = var.volumes[count.index].volume_size
+  type              = var.volumes[count.index].volume_type
+  iops              = var.volumes[count.index].iops
+  kms_key_id        = var.volumes[count.index].kms_key_id
+  tags              = var.volumes[count.index].tags
+
+}
+
+resource "aws_volume_attachment" "wsfc_node1_volume1_attachment" {
+  count       = length(var.volumes)
+  device_name = var.volumes[count.index].device_name
+  volume_id   = aws_ebs_volume.volume[count.index].id
+  instance_id = aws_instance.ec2_instance.id
+}
+
+resource "aws_network_interface" "networks" {
+  count           = length(var.networks)
+  subnet_id       = var.networks[count.index].subnet_id
+  private_ips     = var.networks[count.index].private_ips
+  security_groups = var.networks[count.index].security_groups
+
+  attachment {
+    instance     = aws_instance.ec2_instance.id
+    device_index = var.networks[count.index].device_index
+  }
+}
---
+++ ec2/variables.tf
@@ -0,0 +1,167 @@
+variable "ami" {
+  type = string
+}
+
+variable "instance_name" {
+  description = "NetBIOS name of the domain"
+  type        = string
+  default     = ""
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_pair" {
+  type = string
+}
+
+variable "secuity_groups_ids" {
+    type = list(string)
+    description = "(optional) ids of the SGs to be attahced to the instance"
+    default = []
+}
+
+variable "create_sg" {
+    type        = bool
+    description = "(optional) create sg or not"
+    default     = false
+}
+
+variable "sg_name" {
+    type        = string
+    description = "(optional) security group name"
+    default     = ""
+}
+
+variable "disable_api_termination" {
+  type    = bool
+  default = false
+}
+
+variable "root_volume_size" {
+  type = number
+}
+
+variable "root_volume_type" {
+  type    = string
+  default = ""
+}
+
+variable "machine_iam_policies" {
+  type = list(object(
+  	{
+  		policy_name = string,
+  	    statement = list(object({
+  		    action   = list(string)
+  		    effect   = string
+  		    resource = string
+  		}))
+  	}
+  ))
+  default = []
+}
+
+variable "ingress_roles" {
+  type = list(object({
+          description       = string,
+          from_port         = string,
+          to_port           = string,
+          protocol          = string,
+          cidr_blocks       = list(string),
+          ipv6_cidr_blocks  = list(string)
+      }
+  ))
+  default = []
+}
+
+variable "egress_roles" {
+  type = list(object({
+          description       = string,
+          from_port         = string,
+          to_port           = string,
+          protocol          = string,
+          cidr_blocks       = list(string),
+          ipv6_cidr_blocks  = list(string)
+      }
+  ))
+  default = []
+}
+
+variable "vpc_id" {
+  description = "The id of the vpc where the SGs created ."
+  type        = string
+}
+
+variable "associate_public_ip_address" {
+  type    = bool
+  default = false
+}
+
+variable "secondary_private_ips" {
+  type    = list(string)
+  default = []
+}
+
+variable "root_volume_delete_on_termination" {
+  type    = bool
+  default = false
+}
+
+variable "private_ip" {
+  type        = string
+  description = "private ip"
+  default     = ""
+}
+
+variable "script" {
+  type    = string
+  default = ""
+}
+variable "common_tags" {
+  type = map(string)
+}
+
+variable "extra_tags" {
+  type = map(string)
+}
+
+variable "volumes" {
+  type = list(object({
+    device_name           = string,
+    delete_on_termination = bool,
+    volume_size           = number,
+    volume_type           = string,
+    encrypted             = bool,
+    kms_key_id            = string,
+    iops                  = string,
+    tags                  = object({})
+  }))
+  default = []
+}
+
+variable "networks" {
+  type = list(object({
+    subnet_id       = string
+    private_ips     = list(string)
+    security_groups = list(string)
+    device_index    = number
+  }))
+  default = []
+}
+
+variable "disks_list" {
+  description = "Use each disk,Letter,Label then '|' to add another disk"
+  type        = string
+  default     = ""
+}
+
+variable "is_linux" {
+  type    = bool
+  default = false
+}
+
+variable "subnet_id" {
+  type = string
+  description = "subnet id for creation of the EC2 instance"
+}
