d8c5ca31896726a4cf9404c9227136af9b3c3482
Alex Kogan (Onica) <akogan@onica.com>
2021-12-23 17:00:07 -0500

    adding map-migrated tag

--- ec2/main.tf
+++ ec2/main.tf
@@ -76,7 +76,7 @@ resource "aws_instance" "ec2_instance" {
   subnet_id                   = var.subnet_id
   private_ip                  = var.private_ip
   secondary_private_ips       = var.secondary_private_ips
-  vpc_security_group_ids      = var.create_sg ? concat([aws_security_group.ec2_security_group[0].id], var.security_groups_ids) : var.security_groups_ids 
+  vpc_security_group_ids      = var.create_sg ? concat([aws_security_group.ec2_security_group[0].id], var.security_groups_ids) : var.security_groups_ids
   key_name                    = var.key_pair
   associate_public_ip_address = var.associate_public_ip_address
   disable_api_termination     = var.disable_api_termination
@@ -89,7 +89,7 @@ resource "aws_instance" "ec2_instance" {
     device_name           = lookup(var.root_block_device, "device_name", null)
     encrypted             = lookup(var.root_block_device, "encrypted", null)
     iops                  = lookup(var.root_block_device, "iops", null)
-    tags                  = lookup(var.root_block_device, "tags", null)
+    tags                  = lookup(var.root_block_device, "tags", var.common_tags)
   }
 
   tags = merge(
