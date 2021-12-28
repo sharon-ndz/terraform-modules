4781cd5b276417b036307c961c62620e3dff4a08
halfadywageeh <fady.wageeh@halliburton.com>
2021-12-28 18:16:34 +0200

    add necessary outputs for VPC module

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -4,8 +4,32 @@ output "vpc_id" {
   value       = aws_vpc.vpc.id
 }
 
-# VPC ID
+# Public Subnets IDs
 output "public_subnets_ids" {
   description = "public subnets ids"
   value       = aws_subnet.public_subnets.*.id
+}
+
+# LB Subnets IDs
+output "lb_subnets_ids" {
+  description = "lb subnets ids"
+  value       = aws_subnet.private_lb_subnets.*.id
+}
+
+# App Subnets IDs
+output "app_subnets_ids" {
+  description = "app subnets ids"
+  value       = aws_subnet.private_app_subnets.*.id
+}
+
+# Data Subnets IDs
+output "data_subnets_ids" {
+  description = "data subnets ids"
+  value       = aws_subnet.private_data_subnets.*.id
+}
+
+# Services Subnets IDs
+output "services_subnets_ids" {
+  description = "services subnets ids"
+  value       = aws_subnet.private_services_subnets.*.id
 }
