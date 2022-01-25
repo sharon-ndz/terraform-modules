d654bfe433be2cfa779b7a916dcc28a793c292ec
halfadywageeh <fady.wageeh@halliburton.com>
2022-01-25 14:08:46 +0200

    don't use name prefix for asg by default

--- asg/variables.tf
+++ asg/variables.tf
@@ -114,7 +114,7 @@ variable "name" {
 variable "use_name_prefix" {
   description = "Determines whether to use `name` as is or create a unique name beginning with the `name` as the prefix"
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "instance_name" {
