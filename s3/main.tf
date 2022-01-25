b56ed5b523bc4baa745065f29e97ac33f1666168
halfadywageeh <fady.wageeh@halliburton.com>
2022-01-25 23:22:24 +0200

    handle condition inside bucket policy

--- s3/main.tf
+++ s3/main.tf
@@ -64,7 +64,7 @@ data "aws_iam_policy_document" "this" {
 
 
         dynamic "condition" {
-          for_each = statement.value.condition
+          for_each = statement.value.condition != null ? statement.value.condition : []
 
           content {
             test     = lookup(condition.value, "test", null)
