adf7366ff113be9c5cb0ac2dc10dd2115df51ddd
Makary Hanna <msamy@onica.com>
2022-01-29 15:41:32 +0200

    add iam role for aws backup to have the necessary permission

--- aws_backup/variables.tf
+++ aws_backup/variables.tf
@@ -9,7 +9,6 @@ variable "backup_schedule" { type = string }
 variable "backup_plan_start_window" { type = string }
 variable "backup_plan_completion_window" { type = string }
 variable "retention_period" { type = string }
-variable "aws_backup_role_arn" { type = string }
 variable "aws_backup_selection_name" { type = string }
 variable "resourses_selection_tag_key" { type = string }
 variable "resourses_selection_tag_value" { type = string }
