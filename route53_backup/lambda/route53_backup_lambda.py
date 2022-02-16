234646bdd4f72342094fd9c96c295ffe2578cb2c
Makary Hanna <msamy@onica.com>
2022-02-16 14:32:27 +0200

    modify some code to adjust te huge number of hosted zones

--- route53_backup/lambda/route53_backup_lambda.py
+++ route53_backup/lambda/route53_backup_lambda.py
@@ -4,6 +4,8 @@ import os
 from datetime import date
 import shutil
 from pathlib import Path
+import time
+from botocore.config import Config
 
 
 
@@ -12,15 +14,16 @@ def handler(event, context):
     route53BackupPath = "/tmp/r53_backup"
     Path(route53BackupPath).mkdir(parents=True, exist_ok=True)
     print (today," Backup all Route53 zones and resource records.")
-    route53Client = boto3.client('route53')
-    listResponse = route53Client.list_hosted_zones()
-    for hostedZone in listResponse['HostedZones']:
-       Id= hostedZone['Id']
-       name = hostedZone['Name']
-       response = route53Client.list_resource_record_sets(HostedZoneId=Id)
-       file = open(route53BackupPath+"/"+name+"json","w")
-       file.write(json.dumps(response))
-       file.close()
+    route53Client = boto3.client('route53', config=Config(retries={'max_attempts': 20}))
+    r53Paginator = route53Client.get_paginator('list_hosted_zones')
+    for page in r53Paginator.paginate():
+      for hostedZone in page['HostedZones']:
+         Id= hostedZone['Id']
+         name = hostedZone['Name']
+         response = route53Client.list_resource_record_sets(HostedZoneId=Id)
+         file = open(route53BackupPath+"/"+name+"json","w")
+         file.write(json.dumps(response))
+         file.close()
     shutil.make_archive("/tmp/route53Backup", 'zip', route53BackupPath)
     s3 = boto3.resource('s3')
     backupS3Name = os.getenv('s3_backup_bucket_name')
