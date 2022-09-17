d10b6fe162bca285120d4b65ed8d23f726830472
vsimonov-tribute <viktor.simonov@tributetech.com>
2022-09-17 04:31:06 +0300

    Removed tags from bucket access block in Codebuild module

--- codebuild/main.tf
+++ codebuild/main.tf
@@ -82,7 +82,6 @@ resource "aws_s3_bucket_public_access_block" "default" {
   block_public_policy     = var.block_public_policy
   ignore_public_acls      = var.ignore_public_acls
   restrict_public_buckets = var.restrict_public_buckets
-  tags                    = var.tags
 }
 
 resource "aws_iam_role" "default" {
