6fc12df6ea50d6b5e73c5d598611d7898dd2ecba
sharon-ndz <sharon@ndz.co>
2025-06-10 16:57:09 +0530

    Delete route53_backup directory

--- route53_backup/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "region" {}
-variable "tags" {
-  description = "tags to propogate to all supported resources"
-  type        = map(string)
-}
-variable "s3_backup_bucket_name" {}
-variable "env" {}
-variable "cronExpression" {}
