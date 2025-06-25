e025c07d1663e6c8237009af8c02f77d44b1fdce
sharon-ndz <sharon@ndz.co>
2025-06-26 00:17:13 +0530

    Update locals.tf

--- api-gateway/locals.tf
+++ api-gateway/locals.tf
@@ -1,4 +1,5 @@
 locals {
-  status_codes = ["200", "400", "403", "404", "500"]
+  status_codes       = ["200", "400", "403", "404", "500"]
+  default_code       = "200"
+  error_status_codes = [for code in local.status_codes : code if code != local.default_code]
 }
-
